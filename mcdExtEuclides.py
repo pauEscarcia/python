48c19a6759869c8cafcccc415151c3cda620fdf0
PauEscarcia <paulina_151090@hotmail.com>
2016-04-14 22:43:03 -0500

    MCD Extendido Euclides

--- mcdExtEuclides.py
+++ mcdExtEuclides.py
@@ -3,13 +3,18 @@ def intercambiar(a,b):
 	a=b
 	b=a
 def egcd(a, b):
-    x, y, u, v = 0, 1, 1, 0
-    while a != 0:
-        q, r = b // a, b % a
-        m, n = x - u * q, y - v * q
-        b, a, x, y, u, v = a, r, u, v, m, n
-        print a 
-    return b, x, y
+	x=0
+	y=1
+	u=1
+	v=1
+	if a<b:
+		intercambiar(a,b)
+	while a != 0:
+		q, r = b // a, b % a
+		m, n = x - u * q, y - v * q
+		b, a, x, y, u, v = a, r, u, v, m, n
+		print a
+	return b, x, y
 
 
 egcd(1547,560)
