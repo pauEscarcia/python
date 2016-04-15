cbdd533b14b1a611dee9bc265f3474505b343b20
PauEscarcia <paulina_151090@hotmail.com>
2016-04-15 01:23:22 -0500

    Algoritmo Extendido Euclides

--- mcdExtEuclides.py
+++ mcdExtEuclides.py
@@ -1,14 +1,9 @@
 #algoritmo extendido 
-def intercambiar(a,b):
-	a=b
-	b=a
 def egcd(a, b):
 	x=0
 	y=1
 	u=1
 	v=1
-	if a<b:
-		intercambiar(a,b)
 	while a != 0:
 		q, r = b // a, b % a
 		m, n = x - u * q, y - v * q
