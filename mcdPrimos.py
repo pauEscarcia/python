82bde9a7885c2040fe174cde68fb06ab0368126b
PauEscarcia <paulina_151090@hotmail.com>
2016-04-14 21:36:47 -0500

    mcdPrimos

--- mcdPrimos.py
+++ mcdPrimos.py
@@ -2,6 +2,14 @@ def intercambiar(a,b):
 	a=b
 	b=a
 
+def primo(num):
+    if num < 2: 
+       return False
+    for i in range(2, num): 
+        if num % i == 0:    #si el resto da 0 no es primo, por lo tanto devuelve Falso
+            return False
+    return True    
+
 def mcd(a,b):
 	if a<b:
 		intercambiar(a,b)
@@ -14,9 +22,26 @@ def mcd(a,b):
 		r= n -(d*(n/d))
 		print d
 
-mcd(560,1547)
+def mcdprimo(a,b):
+	cont=0
+	if a<b:
+		intercambiar(a,b)
+	n=a
+	d=b
+	r= n-(d * (n/d))
+	while r!=0:
+		n=d
+		d=r
+		r= n -(d*(n/d))
+		print d
+		if primo(d)==True:
+			cont += 1  
+    	print "Hay", cont, "numeros primos"  
+    	print "Que son: ",d 	        
+            
+mcdprimo(1547,560)
 
-#otra forma de sacarlos (:
+#otra forma de sacar mcd es con modulo (:
 def gcd(a,b):
 	if a<b:
 		intercambiar (a,b)
@@ -28,4 +53,3 @@ def gcd(a,b):
 		b=r
 		print a 
 			
-#gcd(1547,560)
